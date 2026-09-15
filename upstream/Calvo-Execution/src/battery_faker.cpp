d74dbfd54cb2a3f01aa2c5a00dcc456e40ae493d
Haojie Lei <147257503+CquL@users.noreply.github.com>
2026-09-15 11:35:55 -0400

    feat: add Calvo execution upstream snapshot

---
+++ upstream/Calvo-Execution/src/battery_faker.cpp
@@ -0,0 +1,205 @@
+#include "ros/ros.h"
+#include <ros/package.h>
+#include "yaml-cpp/yaml.h"
+
+#include <string>
+#include <map>
+
+#include "mission_planner/classes.h"
+#include "mission_planner/BatteryControl.h"
+
+#include "uav_abstraction_layer/State.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "sensor_msgs/BatteryState.h"
+
+class BatteryFaker{
+  private:
+    std::string id_;
+
+    std::string battery_mode_;
+    int mode_;
+    float battery_increase_;
+    float battery_decrease_;
+		int state_;
+
+    classes::Position position_;
+
+		std::string pose_topic_;
+		std::string state_topic_;
+    std::string config_file;
+    std::map <std::string, std::map <std::string, classes::Position>> known_positions_;
+
+    //Node Handlers
+    ros::NodeHandle nh_;
+
+    // Subscribers: battery_control, ual (position), ual(state)
+    ros::Subscriber control_sub_; 
+    ros::Subscriber position_sub_; 
+		ros::Subscriber state_sub_;
+
+    // Publishers
+    ros::Publisher battery_pub_;
+    sensor_msgs::BatteryState battery_;
+    ros::Rate loop_rate_;
+
+    //Service Server
+
+  public:
+    BatteryFaker() : loop_rate_(0.2), battery_increase_(0.001), battery_decrease_(0.001){
+      // Load battery mode. Default: static
+      ros::param::param<std::string>("~battery_mode", battery_mode_, "static");
+
+      if (battery_mode_ == "recharge_anywhere")
+        mode_ = 1;
+      else if (battery_mode_ == "recharge_in_base")
+        mode_ = 2;
+      else if (battery_mode_ == "only_discharge")
+        mode_ = 3;
+      else // static
+        mode_ = 0;
+
+      ros::param::param<std::string>("~id", id_, "i");
+      ros::param::param<std::string>("~pose_topic", pose_topic_, "/" + id_ + "/ual/pose");
+      ros::param::param<std::string>("~state_topic", state_topic_, "/" + id_ + "/ual/state");
+
+      //load of known position and human targets known positions
+      std::string path = ros::package::getPath("mission_planner");
+      ros::param::param<std::string>("~config_file", config_file, path + "/config/conf.yaml");
+
+      ROS_INFO("Reading config file...");
+      readConfigFile(config_file);
+
+      battery_pub_ = nh_.advertise<sensor_msgs::BatteryState>("/" + id_ + "/battery_fake", 1);
+
+      control_sub_ = nh_.subscribe("/" + id_ + "/battery_fake/control", 1, &BatteryFaker::controlCallback, this);
+
+      position_sub_ = nh_.subscribe(pose_topic_, 1, &BatteryFaker::positionCallbackUAL, this);
+      state_sub_ = nh_.subscribe(state_topic_, 1, &BatteryFaker::stateCallbackUAL, this);
+
+      ROS_INFO("READY");
+      battery_.percentage = 1;
+      loop_rate_.reset();
+      while(ros::ok())
+      {
+        ros::spinOnce();
+        update_battery();
+        loop_rate_.sleep();
+      }
+    }
+    ~BatteryFaker(){}
+    void update_battery(){
+      int flag;
+      switch(mode_)
+      {
+        case 1: //Recharge anywhere when landed
+          switch(state_)
+          {
+            case 1: //LANDED_DISARMED
+            case 2: //LANDED_ARMED
+              battery_.percentage = battery_.percentage + battery_increase_;
+              if(battery_.percentage > 1)
+                battery_.percentage = 1;
+              break;
+            case 3: //TAKING_OFF
+            case 4: //FLYING_AUTO
+            case 5: //FLYING_MANUAL
+            case 6: //LANDING
+              battery_.percentage = battery_.percentage - battery_decrease_;
+              if(battery_.percentage < 0)
+                battery_.percentage = 0;
+              break;
+            case 0: //UNINITIALIZED
+            default:
+              break;
+          }
+        case 2: //Recharge only in recharging base
+          switch(state_)
+          {
+            case 1: //LANDED_DISARMED
+            case 2: //LANDED_ARMED
+              flag = 0;
+              for(auto& charging_station : known_positions_["charging_stations"])
+              {
+                if(classes::distance(position_, charging_station.second) < 0.5)
+                  flag = 1;
+              }
+              if(flag)
+              {
+                battery_.percentage = battery_.percentage + battery_increase_;
+                if(battery_.percentage > 1)
+                  battery_.percentage = 1;
+              }
+              break;
+            case 3: //TAKING_OFF
+            case 4: //FLYING_AUTO
+            case 5: //FLYING_MANUAL
+            case 6: //LANDING
+              battery_.percentage = battery_.percentage - battery_decrease_;
+              if(battery_.percentage < 0)
+                battery_.percentage = 0;
+              break;
+            case 0: //UNINITIALIZED
+            default:
+              break;
+          }
+        case 3: //Recharge disabled
+          switch(state_)
+          {
+            case 3: //TAKING_OFF
+            case 4: //FLYING_AUTO
+            case 5: //FLYING_MANUAL
+            case 6: //LANDING
+              battery_.percentage = battery_.percentage - battery_decrease_;
+              if(battery_.percentage < 0)
+                battery_.percentage = 0;
+              break;
+            case 0: //UNINITIALIZED
+            case 1: //LANDED_DISARMED
+            case 2: //LANDED_ARMED
+            default:
+              break;
+          }
+        case 0: //Battery Static
+        default:
+          break;
+      }
+      battery_pub_.publish(battery_);
+      //ROS_INFO_STREAM("Mode: " << mode_ << "\tUAV State: " << state_ << "\tPercentage: " << battery_.percentage);
+    }
+    void readConfigFile(std::string config_file){
+      YAML::Node yaml_config = YAML::LoadFile(config_file);
+      if(yaml_config["positions"])
+      {
+        for(auto const& group : yaml_config["positions"])
+        {
+          for(auto const& position : group.second)
+          {
+            known_positions_[group.first.as<std::string>()][position.first.as<std::string>()] = 
+              classes::Position(position.first.as<std::string>(), position.second['x'].as<float>(), 
+                  position.second['y'].as<float>(), position.second['z'].as<float>());
+          }
+        }
+      }
+    }
+    //Callbacks
+    void controlCallback(const mission_planner::BatteryControl& control){
+      if(control.percentage != -1)
+        battery_.percentage = control.percentage;
+      mode_ = control.mode;
+      if(control.battery_increase >= 0)
+        battery_increase_= control.battery_increase;
+      if(control.battery_decrease >= 0)
+        battery_decrease_= control.battery_decrease;
+    }
+    void positionCallbackUAL(const geometry_msgs::PoseStamped& pose){
+      position_.update(pose.pose.position.x, pose.pose.position.y, pose.pose.position.z);
+    }
+    void stateCallbackUAL(const uav_abstraction_layer::State& state){state_ = state.state;}
+};
+
+int main(int argc, char **argv){
+  ros::init(argc, argv, "battery_faker");
+
+  BatteryFaker battery_faker;
+  return 0;
+}
---
+++ upstream/Calvo-Execution/src/ist_ugv_faker.cpp
@@ -0,0 +1,160 @@
+#include "ros/ros.h"
+#include <ros/package.h>
+
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib_tutorials/FibonacciAction.h>
+
+#include "mission_planner/RequestMobileChargingStationAction.h"
+#include "mission_planner/DoCloserInspectionAction.h"
+
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib_tutorials/FibonacciAction.h>
+
+#include "geographic_msgs/GeoPoseStamped.h"
+
+#include <cmath>
+
+class ISTugvFaker{
+  protected:
+    ros::NodeHandle nh_;
+    actionlib::SimpleActionServer<mission_planner::RequestMobileChargingStationAction> mobile_station_as_;
+    actionlib::SimpleActionServer<mission_planner::DoCloserInspectionAction> closer_inspection_as_;
+
+    mission_planner::RequestMobileChargingStationResult mobile_station_result_;
+    mission_planner::DoCloserInspectionResult closer_inspection_result_;
+
+    // UGV Pose Fakers Publishers
+    ros::Publisher atrvjr_pose_pub_;
+    ros::Publisher jackal_pose_pub_;
+    geographic_msgs::GeoPoseStamped atrvjr_pose_; 
+    geographic_msgs::GeoPoseStamped jackal_pose_;
+    geographic_msgs::GeoPoseStamped north_position_;
+    geographic_msgs::GeoPoseStamped south_position_;
+    geographic_msgs::GeoPoseStamped center_;
+    float latitude_increment_;
+    float angular_velocity_;
+    float angle_increment_;
+    float current_angle_;
+    ros::Rate pose_rate_;
+    std::string atrvjr_direction_;
+    std::string jackal_direction_;
+
+  public:
+    ISTugvFaker() : pose_rate_(3), // f = 3 -> T = 0.333... seconds
+      mobile_station_as_(nh_, "/jackal0/cooperation_use/request_mobile_charging_station", boost::bind(&ISTugvFaker::mobileStationCB, this, _1), false),
+      closer_inspection_as_(nh_, "/atrvjr/cooperation_use/do_closer_inspection", boost::bind(&ISTugvFaker::closerInspectionCB, this, _1), false) {
+        mobile_station_as_.start();
+        closer_inspection_as_.start();
+
+        atrvjr_pose_pub_ = nh_.advertise<geographic_msgs::GeoPoseStamped>("/atrvjr/geopose", 1);
+        jackal_pose_pub_ = nh_.advertise<geographic_msgs::GeoPoseStamped>("/jackal0/geopose", 1);
+
+        // Initial atrvjr and jackal movement directions
+        atrvjr_direction_ = "right";
+        jackal_direction_ = "up";
+
+        // Define north and south points for jackal trajectory
+        // Latitude  - y
+        // Longitude - x
+        // Altitude  - z
+        north_position_.pose.position.latitude = 38.54121161489917;
+        north_position_.pose.position.longitude = -7.961711602856107;
+        north_position_.pose.position.altitude = 230.1384181595744;
+        south_position_.pose.position.latitude = 38.54106489217978;
+        south_position_.pose.position.longitude = -7.961711602856107;
+        south_position_.pose.position.altitude = 230.1384181595744;
+
+        // Define center coordinates for atrvjr circular trajectory
+        center_.pose.position.latitude = 38.5410856346297;
+        center_.pose.position.longitude = -7.961538763126298;
+        center_.pose.position.altitude = 230.0598269717272;
+
+        // Compute the circular trajectory parameters
+        angular_velocity_ = 2 * 3.141592654 / 30; // w = 2 * pi / T
+        angle_increment_ = angular_velocity_ * 1; // AO = w * At
+        current_angle_ = 0;
+
+        // Compute the latitude increment per position update interval
+        // 15 meters, let's say 7'5 seconds to travel those 15 meters, 0'2 seconds per positions update
+        // 38 updates per direction
+        latitude_increment_ = (north_position_.pose.position.latitude - south_position_.pose.position.latitude) / 38;
+
+        // Position: solar pannel row 14 column 2
+        // atrvjr_pose_.pose.position.latitude = 38.54143688611777;
+        // atrvjr_pose_.pose.position.longitude = -7.961302628908529;
+        // atrvjr_pose_.pose.position.altitude = 227.61875915527344;
+
+        //Initial position: south
+        jackal_pose_.pose.position.latitude = south_position_.pose.position.latitude;
+        jackal_pose_.pose.position.longitude = south_position_.pose.position.longitude;
+        jackal_pose_.pose.position.altitude = south_position_.pose.position.altitude;
+
+        atrvjr_pose_.pose.position.altitude = center_.pose.position.altitude;
+
+        pose_rate_.reset();
+        while(ros::ok())
+        {
+          ros::spinOnce();
+          UGVPositionUpdater();
+          atrvjr_pose_pub_.publish(atrvjr_pose_);
+          jackal_pose_pub_.publish(jackal_pose_);
+          pose_rate_.sleep();
+        }
+      }
+
+    ~ISTugvFaker(void){}
+
+    void UGVPositionUpdater() {
+      // Jackal will move in a vertical line in the central path at the north of the origin
+      // Point 1: x==0, y==10: -7.961712761304593,38.54106489217978,230.1931775972878
+      // Point 2: x==0, y==25: -7.961711602856107,38.54121161489917,230.1384181595744
+      if (jackal_direction_ == "up") {
+        jackal_pose_.pose.position.latitude = jackal_pose_.pose.position.latitude + latitude_increment_;
+        if (jackal_pose_.pose.position.latitude > north_position_.pose.position.latitude) {
+          jackal_direction_ = "down";
+        }
+      }
+      else {
+        jackal_pose_.pose.position.latitude = jackal_pose_.pose.position.latitude - latitude_increment_;
+        if (jackal_pose_.pose.position.latitude < south_position_.pose.position.latitude) {
+          jackal_direction_ = "down";
+        }
+      }
+
+      // atrvjr will move in a square around the building
+      // Corner 1: x==0,  y==30:   -7.961710021346433,38.54128355993739,229.9654475267011
+      // Corner 2: x==20, y==30:   -7.961515416900085,38.54128629006915,229.5583745517417
+      // Corner 3: x==20, y==47.5: -7.961515302176315,38.54142284054371,229.1205587457127
+      // Corner 4: x==0,  y==47.5: -7.961710530654779,38.54142927235737,229.567600993049
+
+      // atrvjr will move in a circle around a coordinate center point
+      current_angle_ += angle_increment_;
+      if (current_angle_ > 2 * 3.141592654)
+        current_angle_ = 0;
+      atrvjr_pose_.pose.position.latitude = center_.pose.position.latitude   + 0.00005 * sin(current_angle_);
+      atrvjr_pose_.pose.position.longitude = center_.pose.position.longitude + 0.00005 * cos(current_angle_);
+    }
+
+    void mobileStationCB(const mission_planner::RequestMobileChargingStationGoalConstPtr &goal) {
+      mobile_station_result_.success = true;
+      ROS_INFO("Requested mobile charging station. Returning SUCCESS");
+      mobile_station_as_.setSucceeded(mobile_station_result_);
+    }
+
+    void closerInspectionCB(const mission_planner::DoCloserInspectionGoalConstPtr &goal) {
+      closer_inspection_result_.success = true;
+      ROS_INFO("Requested closer inspection. Returning SUCCESS");
+      closer_inspection_as_.setSucceeded(closer_inspection_result_);
+    }
+};
+
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "ist_ugv_faker");
+
+  ISTugvFaker ist_ugv_faker;
+  ROS_INFO("Ending");
+
+  return 0;
+}
